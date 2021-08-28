7ccb7e6ad425bfa8fc8f87f956336199ac150b87
arsh-purewal <89690799+arsh-purewal@users.noreply.github.com>
2021-08-28 17:27:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+#importing the libraries
+import pickle
+import numpy
+from flask import Flask, request, render_template
+
+#Global Variables
+app = Flask(__name__)
+loadedModel = pickle.load(open('car_price.pkl', 'rb'))
+
+#User defined Functions
+@app.route('/', methods=['GET'])
+def Home():
+    return render_template('car_price_prediction.html')
+
+@app.route('/prediction', methods= ['POST'])
+def predict():
+    Max_Power = int(request.form['Max_Power'])
+    Torque = int(request.form['Torque'])
+    Engine = int(request.form['Engine'])   #name shoul match with html page
+    year = int(request.form['year'])
+
+    prediction = loadedModel.predict([[Max_Power, Torque, Engine, year]])
+    prediction    
+    
+    return render_template('car_price_prediction.html', diagnosis_output=prediction)
+
+#main function
+if __name__ == "__main__":
+    app.run(debug = True)
