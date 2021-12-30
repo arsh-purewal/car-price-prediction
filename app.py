b4941c6dc1c23eb659bb79617a2b8ef43f42ee5b
arsh-purewal <89690799+arsh-purewal@users.noreply.github.com>
2021-12-30 09:57:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ def predict():
     Max_Power = int(request.form['Max_Power'])
     Torque = int(request.form['Torque'])
     Engine = int(request.form['Engine'])   #name shoul match with html page
-    year = int(request.form['year'])
+    Year = int(request.form['Year'])
 
-    prediction = loadedModel.predict([[Max_Power, Torque, Engine, year]])
+    prediction = loadedModel.predict([[Max_Power, Torque, Engine, Year]])
     prediction    
     
     return render_template('index.html', Predictd_Price=prediction)
